b34227f728ad367e7e39335c29221a5662903166
takay <takay@takayanagi-ms>
2019-11-06 20:23:53 +0900

    初期化データのエクスポート機能を追加。

---
+++ HTML/META-INF/resources/dataforms/app/enumtype/page/EnumQueryForm.js
@@ -0,0 +1,44 @@
+/**
+ * @fileOverview {@link EnumQueryForm}クラスを記述したファイルです。
+ */
+
+/**
+ * @class EnumQueryForm
+ *
+ * @extends QueryForm
+ */
+class EnumQueryForm extends QueryForm {
+	/**
+	 * コンストラクタ。
+	 */
+	constructor() {
+		super();
+	}
+
+	/**
+	 * HTMLエレメントとの対応付けを行います。
+	 */
+	attach() {
+		super.attach();
+		if (currentPage.userInfo.userLevel == "developer") {
+			this.find("#exportInitDataButton").click(() => {
+				this.exportInitData()
+			});
+		} else {
+			this.find("#exportInitDataButton").remove();
+		}
+	}
+
+	/**
+	 * データのエクスポートを行います。
+	 */
+	exportInitData() {
+		currentPage.confirm(null, MessagesUtil.getMessage("message.dexportAsInitialDataConfirm"), () => {
+			this.submit("export", (data) => {
+				currentPage.alert(null, data.result);
+			});
+		});
+	}
+
+}
+
--- src/dataforms/app/enumtype/page/EnumQueryForm.java
+++ src/dataforms/app/enumtype/page/EnumQueryForm.java
@@ -1,8 +1,15 @@
 package dataforms.app.enumtype.page;
 
+import java.util.Map;
+
+import dataforms.annotation.WebMethod;
 import dataforms.app.enumtype.dao.EnumTable;
 import dataforms.controller.QueryForm;
+import dataforms.devtool.base.page.DeveloperPage;
+import dataforms.devtool.db.dao.TableManagerDao;
 import dataforms.field.base.Field.MatchType;
+import dataforms.response.JsonResponse;
+import dataforms.util.MessagesUtil;
 
 
 
@@ -19,4 +26,26 @@ public class EnumQueryForm extends QueryForm {
 		this.addField(table.getEnumGroupCodeField()).setMatchType(MatchType.PART);
 		this.addField(table.getMemoField()).setMatchType(MatchType.PART);
 	}
+
+	/**
+	 * 列挙型関連テーブルのエクスポートを行います。
+	 * @param p パラメータ。
+	 * @return Json形式のエクスポート。
+	 * @throws Exception 例外。
+	 */
+	@WebMethod
+	public JsonResponse export(final Map<String, Object> p) throws Exception {
+		JsonResponse ret = null;
+		if (this.getPage().checkUserAttribute("userLevel", "developer")) {
+			TableManagerDao dao = new TableManagerDao(this);
+			String initialDataPath =  DeveloperPage.getExportInitalDataPath(this.getPage()); //DeveloperPage.getWebSourcePath() + "/WEB-INF/initialdata";
+			dao.exportData("dataforms.app.enumtype.dao.EnumTable", initialDataPath);
+			dao.exportData("dataforms.app.enumtype.dao.EnumNameTable", initialDataPath);
+			ret = new JsonResponse(JsonResponse.SUCCESS, MessagesUtil.getMessage(this.getPage(), "message.initializationdatacreated"));
+		} else {
+			ret = new JsonResponse(JsonResponse.INVALID, MessagesUtil.getMessage(this.getPage(), "error.permission"));
+		}
+		return ret;
+	}
+
 }

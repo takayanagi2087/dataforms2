2a16da2c1d0d6955887e8a93b3b35da89e10ea8b
takay <takay@takayanagi-ms>
2020-04-02 17:38:42 +0900

    検索条件を修正。

--- src/sample/page/MaterialMasterQueryForm.java
+++ src/sample/page/MaterialMasterQueryForm.java
@@ -3,7 +3,6 @@ package sample.page;
 import dataforms.controller.QueryForm;
 import dataforms.field.base.Field.MatchType;
 import sample.dao.MaterialMasterTable;
-import sample.field.UnitPriceField;
 
 
 
@@ -16,10 +15,11 @@ public class MaterialMasterQueryForm extends QueryForm {
 	 */
 	public MaterialMasterQueryForm() {
 		MaterialMasterTable table = new MaterialMasterTable();
-		this.addField(table.getMaterialNameField()).setMatchType(MatchType.FULL);
-		this.addField(table.getMaterialUnitField()).setMatchType(MatchType.FULL);
-		this.addField(new UnitPriceField(MaterialMasterTable.Entity.ID_UNIT_PRICE + "From")).setMatchType(MatchType.RANGE_FROM).setComment("単価(from)");
-		this.addField(new UnitPriceField(MaterialMasterTable.Entity.ID_UNIT_PRICE + "To")).setMatchType(MatchType.RANGE_TO).setComment("単価(to)");
-		this.addField(table.getMemoField()).setMatchType(MatchType.FULL);
+		this.addField(table.getMaterialCodeField()).setMatchType(MatchType.PART);
+		this.addField(table.getMaterialNameField()).setMatchType(MatchType.PART);
+//		this.addField(table.getMaterialUnitField()).setMatchType(MatchType.FULL);
+//		this.addField(new UnitPriceField(MaterialMasterTable.Entity.ID_UNIT_PRICE + "From")).setMatchType(MatchType.RANGE_FROM).setComment("単価(from)");
+//		this.addField(new UnitPriceField(MaterialMasterTable.Entity.ID_UNIT_PRICE + "To")).setMatchType(MatchType.RANGE_TO).setComment("単価(to)");
+		this.addField(table.getMemoField()).setMatchType(MatchType.PART);
 	}
 }

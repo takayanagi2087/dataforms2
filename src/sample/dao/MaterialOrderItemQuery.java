78660bbcd373a47aaa9e7f74e658124b73b29c41
takay <takay@takayanagi-ms>
2020-04-03 09:12:31 +0900

    サンプルの問合せを追加。

---
+++ src/sample/dao/MaterialOrderItemQuery.java
@@ -0,0 +1,52 @@
+package sample.dao;
+
+import dataforms.dao.Query;
+import dataforms.field.base.FieldList;
+
+
+
+/**
+ * 問い合わせクラスです。
+ *
+ */
+public class MaterialOrderItemQuery extends Query {
+	/**
+	 * コンストラクタ.
+	 */
+	public MaterialOrderItemQuery() {
+		this.setDistinct(false);
+		MaterialOrderItemTable materialOrderItemTable = new MaterialOrderItemTable();
+		materialOrderItemTable.setAlias("m");
+		MaterialOrderTable materialOrderTable = new MaterialOrderTable();
+		materialOrderTable.setAlias("j0");
+		MaterialMasterTable materialMasterTable = new MaterialMasterTable();
+		materialMasterTable.setAlias("j1");
+
+		this.setFieldList(new FieldList(
+			materialOrderItemTable.getOrderItemIdField()
+			, materialOrderItemTable.getOrderIdField()
+			, materialOrderItemTable.getSortOrderField()
+			, materialOrderItemTable.getMaterialIdField()
+			, materialOrderItemTable.getOrderPriceField()
+			, materialOrderItemTable.getAmountField()
+			, materialOrderItemTable.getItemMemoField()
+			, materialOrderItemTable.getCreateUserIdField()
+			, materialOrderItemTable.getCreateTimestampField()
+			, materialOrderItemTable.getUpdateUserIdField()
+			, materialOrderItemTable.getUpdateTimestampField()
+			, materialOrderTable.getOrderNoField()
+			, materialOrderTable.getSupplierIdField()
+			, materialOrderTable.getOrderDateField()
+			, materialOrderTable.getMemoField()
+			, materialMasterTable.getMaterialCodeField()
+			, materialMasterTable.getMaterialNameField()
+			, materialMasterTable.getMaterialUnitField()
+			, materialMasterTable.getUnitPriceField()
+			, materialMasterTable.getOrderPointField()
+		));
+		this.setMainTable(materialOrderItemTable);
+		this.addInnerJoin(materialOrderTable);
+		this.addInnerJoin(materialMasterTable);
+
+	}
+}
---
+++ src/sample/dao/MaterialOrderQuery.java
@@ -0,0 +1,43 @@
+package sample.dao;
+
+import dataforms.dao.Query;
+import dataforms.field.base.FieldList;
+
+
+
+/**
+ * 問い合わせクラスです。
+ *
+ */
+public class MaterialOrderQuery extends Query {
+	/**
+	 * コンストラクタ.
+	 */
+	public MaterialOrderQuery() {
+		this.setDistinct(false);
+		MaterialOrderTable materialOrderTable = new MaterialOrderTable();
+		SupplierMasterTable supplierMasterTable = new SupplierMasterTable();
+
+		this.setFieldList(new FieldList(
+			materialOrderTable.getOrderIdField()
+			, materialOrderTable.getOrderNoField()
+			, materialOrderTable.getSupplierIdField()
+			, materialOrderTable.getOrderDateField()
+			, materialOrderTable.getMemoField()
+			, materialOrderTable.getCreateUserIdField()
+			, materialOrderTable.getCreateTimestampField()
+			, materialOrderTable.getUpdateUserIdField()
+			, materialOrderTable.getUpdateTimestampField()
+			, supplierMasterTable.getSupplierCodeField()
+			, supplierMasterTable.getSupplierNameField()
+			, supplierMasterTable.getSupplierKanaNameField()
+			, supplierMasterTable.getPhoneNoField()
+			, supplierMasterTable.getFaxNoField()
+			, supplierMasterTable.getZipCodeField()
+			, supplierMasterTable.getAddressField()
+		));
+		this.setMainTable(materialOrderTable);
+		this.addInnerJoin(supplierMasterTable);
+
+	}
+}

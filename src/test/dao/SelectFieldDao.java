b6f978ee8282122a93ce5f26cadd3d9b3046c7b7
takay <takay@takayanagi-ms>
2020-05-13 21:59:35 +0900

    Dao生成を改善。

--- src/test/dao/SelectFieldDao.java
+++ src/test/dao/SelectFieldDao.java
@@ -61,4 +61,45 @@ public class SelectFieldDao extends QuerySetDao {
 		return null;
 	}
 
+	//
+	// 追加、更新、削除処理を改造する場合は以下のメソッドをオーバーライドしてください。
+	// QuerySetDaoクラスではsingleRecordQuery,multiRecordQueryListに登録された各問合せ
+	// のmainTableのみ操作するようになっています。
+	//
+	/**
+	 * テーブル群を追加します。
+	 * @param data データ。
+	 * @throws Exception 例外。
+	 */
+/*
+	@Override
+	public void insert(final Map<String, Object> data) throws Exception {
+		super.insert(data);
+	}
+*/
+
+	/**
+	 * テーブル群を更新します。
+	 * @param data 更新データ。
+	 * @throws Exception 例外。
+	 */
+/*
+	@Override
+	public void update(final Map<String, Object> data) throws Exception {
+		super.update(data);
+	}
+*/
+
+	/**
+	 * データを削除します。
+	 * @param data データ。
+	 * @throws Exception 例外。
+	 */
+/*
+	@Override
+	public void delete(final Map<String, Object> data) throws Exception {
+		super.delete(data);
+	}
+*/
+
 }

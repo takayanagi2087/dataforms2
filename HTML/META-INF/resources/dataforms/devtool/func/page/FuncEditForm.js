a90e675af85e96de159da809f7ae22e415d8df3f
takay <takay@DAIV>
2022-04-15 20:17:14 +0900

    confirmのasync/await対応。

--- HTML/META-INF/resources/dataforms/devtool/func/page/FuncEditForm.js
+++ HTML/META-INF/resources/dataforms/devtool/func/page/FuncEditForm.js
@@ -23,13 +23,11 @@ class FuncEditForm extends EditForm {
 	/**
 	 * データのエクスポートを行います。
 	 */
-	exportInitData() {
-		var thisForm = this;
-		currentPage.confirm(null, MessagesUtil.getMessage("message.dexportAsInitialDataConfirm"), function() {
-			thisForm.submit("export", function(data) {
-				currentPage.alert(null, data.result);
-			});
-		});
+	async exportInitData() {
+		if (await currentPage.confirm(null, MessagesUtil.getMessage("message.dexportAsInitialDataConfirm"))) {
+			let data = await this.submit("export");
+			currentPage.alert(null, data.result);
+		}
 	}
 
 
@@ -38,14 +36,13 @@ class FuncEditForm extends EditForm {
 	 */
 	toEditMode() {
 		super.toEditMode();
-		var thisForm = this;
-		var table = this.getComponent("funcTable");
-		this.find("[id$='\\.funcPath']").each(function() {
-			var f = thisForm.getComponent($(this).attr(thisForm.getIdAttribute()));
-			logger.log("id=" + f.id + ":" + $(this).val());
-			if ($(this).val().indexOf("/dataforms") == 0) {
-				var field = table.getSameRowField($(this), "funcName");
-				var namef = thisForm.getComponent(field.attr(thisForm.getIdAttribute()));
+		let table = this.getComponent("funcTable");
+		this.find("[id$='\\.funcPath']").each((_, tx) => {
+			let f = this.getComponent($(tx).attr(this.getIdAttribute()));
+			logger.log("id=" + f.id + ":" + $(tx).val());
+			if ($(tx).val().indexOf("/dataforms") == 0) {
+				let field = table.getSameRowField($(tx), "funcName");
+				let namef = this.getComponent(field.attr(this.getIdAttribute()));
 				f.lock(true);
 				namef.lock(true);
 			}
--- HTML/META-INF/resources/dataforms/devtool/query/page/QueryGeneratorQueryResultForm.js
+++ HTML/META-INF/resources/dataforms/devtool/query/page/QueryGeneratorQueryResultForm.js
@@ -22,223 +22,42 @@ class QueryGeneratorQueryResultForm extends QueryResultForm {
 	 */
 	attach() {
 		super.attach();
-		// 独自のボタン(id="webMethodButton")を追加しサーバサイドのメソッドを呼び出す場合は以下の様にしてください。
-/*
-		this.get("webMethodButton").click(() => {
-			this.callWebMethod();
-			return false;
-		});
-*/
 	}
-
-	// 独自のWebメソッドを呼び出す場合は、以下のコードを参考にしてください。
-	/**
-	 * Webメソッドの呼び出しサンプル。
-	 *
-	 */
-/*
-	callWebMethod() {
-		if (this.validate()) {
-			this.submit("webMethod", (r) => {
-				this.parent.resetErrorStatus();
-				if (r.status == ServerMethod.SUCCESS) {
-					// TODO:成功時の処理を記述します。
-					// 応答情報をログ表示
-					logger.dir(r);
-				} else {
-					this.parent.setErrorInfo(this.getValidationResult(r), this);
-				}
-			});
-		}
-	}
-*/
-
-
-	// フォーム単位のバリデーションを行う場合は以下のコメントを参考に実装してください。
-	/**
-	 * フォームのバリデーション。
-	 * <pre>
-	 * フォーム内のフィールド関連チェックを実装します。
-	 * </pre>
-	 */
-/*
-	validateForm() {
-		let list = super.validateForm();
-		if (list.length == 0) {
-			if (エラー判定) {
-				list.push(new ValidationError("fieldId", MessagesUtil.getMessage("error.messagekey")));
-			}
-		}
-		return list;
-	}
-*/
-
-	// フォームの計算処理を行う場合、以下の処理を参考にしてください。
-	/**
-	 * 計算イベント処理を行います。
-	 * <pre>
-	 * 計算イベントフィールドが更新された場合、このメソッドが呼び出されます。
-	 * データ入力時の自動計算が必要な場合このメソッドをオーバーライドしてください。
-	 * </pre>
-	 * @param {jQuery} element イベントが発生した要素。初期表示の時等特定フィールドが要因でない場合はnullが設定されます。
-	 *
-	 */
-/*
-	onCalc(element) {
-	}
-*/
 
 	/**
 	 * SubQueryの生成を行います。
 	 * @param {jQuery} btn ボタン。
 	 */
-	generateSubQuery(btn) {
-		var queryResult = this.getComponent("queryResult");
-		var queryClassName = queryResult.getSameRowField(btn, "fullClassName").text();
+	async generateSubQuery(btn) {
+		let queryResult = this.getComponent("queryResult");
+		let queryClassName = queryResult.getSameRowField(btn, "fullClassName").text();
 		logger.log("queryClassName=" + queryClassName);
-		var m = this.getServerMethod("generateSubQuery");
-		m.execute("queryClass=" + queryClassName, (r) => {
-			if (r.status == ServerMethod.SUCCESS) {
-				currentPage.alert(null, r.result);
-			}
-			this.changePage();
-		});
+		let m = this.getWebMethod("generateSubQuery");
+		let r = await m.execute("queryClass=" + queryClassName);
+		if (r.status == ServerMethod.SUCCESS) {
+			currentPage.alert(null, r.result);
+		}
+		this.changePage();
 	}
 
-	// 問合せ結果リスト中に独自のボタンを追加した場合、以下のコメントを参考にしてイベント処理を実装してください。
 	/**
 	 * 問合せ結果にデフォルトイベント処理を設定します。
 	 */
 	setQueryResultEventHandler() {
 		super.setQueryResultEventHandler();
-		var thisForm = this;
-		var queryResult = this.getComponent("queryResult");
+		let queryResult = this.getComponent("queryResult");
 		// リスト中のボタンに対してイベント処理を追加。
-		this.find("[id$='\.generateSubQueryButton']").click(function() {
-			var sq = queryResult.getSameRowField($(this), "subQuery").text();
+		this.find("[id$='\.generateSubQueryButton']").click(async (ev) => {
+			let sq = queryResult.getSameRowField($(ev.target), "subQuery").text();
 			if (sq.length > 0) {
-				var msg = MessagesUtil.getMessage("message.confirmsubquery");
-				currentPage.confirm(null, msg, () => {
-					thisForm.generateSubQuery($(this));
-				});
+				let msg = MessagesUtil.getMessage("message.confirmsubquery");
+				if (await currentPage.confirm(null, msg)) {
+					this.generateSubQuery($(ev.target));
+				};
 			} else {
-				thisForm.generateSubQuery($(this));
+				this.generateSubQuery($(ev.target));
 			}
 		});
 	}
-
-	// フォームの各種動作をカスタマイズするには以下のメソッドをオーバーライドしてください。
-
-	/**
-	 * 各フィールドにデータを設定します。
-	 * <pre>
-	 * 新規モードの場合、削除ボタンを隠します。
-	 * </pre>
-	 * @param {Object} data フォームデータ.
-	 *
-	 */
-/*
-	setFormData(data) {
-		super.setFormData(data);
-	}
-*/
-
-	/**
-	 * 問い合わせ結果を表示します。
-	 * @param {Object} queryResult 問い合わせ結果。
-	 */
-/*
-	setQueryResult(queryResult) {
-		super.setQueryResult(queryResult);
-	}
-*/
-
-	/**
-	 * 先頭ページに遷移します。
-	 */
-/*
-	topPage() {
-		super.topPage();
-	}
-*/
-	/**
-	 * 末尾ページに遷移します。
-	 */
-/*
-	bottomPage() {
-		super.bottomPage();
-	}
-*/
-
-	/**
-	 * 前ページに遷移します。
-	 */
-/*
-	prevPage() {
-		super.prevPage();
-	}
-*/
-	/**
-	 * 次ページに遷移します。
-	 */
-/*
-	nextPage() {
-		super.nextPage();
-	}
-*/
-
-	/**
-	 * ページの更新を行います。
-	 */
-/*
-	changePage() {
-		super.changePage();
-	}
-*/
-
-	/**
-	 * 選択データを更新します。
-	 */
-/*
-	updateData() {
-		super.updateData();
-	}
-*/
-
-	/**
-	 * 選択データをコピーした新規データを登録します。
-	 */
-/*
-	referData() {
-		super.referData();
-	}
-*/
-	/**
-	 * 選択データの削除を行います。
-	 */
-/*
-	deleteData() {
-		super.deleteData();
-	}
-*/
-
-	/**
-	 * ページ関連情報を設定します。
-	 * @param {Object} queryResult 問い合わせ結果。
-	 */
-/*
-	setPagerInfo(queryResult) {
-		super.setPagerInfo(queryResult);
-	}
-*/
-
-	/**
-	 * ページ関連情報を制御します。
-	 */
-/*
-	controlPager() {
-		super.controlPager();
-	}
-*/
 }
 

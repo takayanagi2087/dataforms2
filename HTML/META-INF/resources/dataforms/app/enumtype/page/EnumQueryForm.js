a6e050ec4af59dba8d356749e81fa256deee92f4
takay <takay@DAIV>
2022-05-31 15:12:08 +0900

    旧バージョンデータの移行に対応。

--- HTML/META-INF/resources/dataforms/app/enumtype/page/EnumQueryForm.js
+++ HTML/META-INF/resources/dataforms/app/enumtype/page/EnumQueryForm.js
@@ -26,8 +26,16 @@ class EnumQueryForm extends QueryForm {
 			this.get("exportInitDataButton").click(() => {
 				this.exportInitData()
 			});
+			this.get("importDataButton").click(() => {
+				this.importInitData();
+			});
+			this.get("importV1DataButton").click(() => {
+				this.importV1InitData();
+			});
 		} else {
 			this.get("exportInitDataButton").remove();
+			this.get("importDataButton").remove();
+			this.get("importV1DataButton").remove();
 		}
 	}
 
@@ -39,6 +47,28 @@ class EnumQueryForm extends QueryForm {
 		if (ret) {
 			let data = await this.submit("export");
 			currentPage.alert(null, data.result);
+		}
+	}
+
+	/**
+	 * データのインポートを行います。
+	 */
+	async importInitData() {
+		let ret = await currentPage.confirm(null, MessagesUtil.getMessage("message.importInitialDataConfirm"));
+		if (ret) {
+			let data = await this.submit("importData");
+			await currentPage.alert(null, data.result);
+		}
+	}
+
+	/**
+	 * ver1.x形式のデータのインポートを行います。
+	 */
+	async importV1InitData() {
+		let ret = await currentPage.confirm(null, MessagesUtil.getMessage("message.importV1InitialDataConfirm"));
+		if (ret) {
+			let data = await this.submit("importV1Data");
+			await currentPage.alert(null, data.result);
 		}
 	}
 }
